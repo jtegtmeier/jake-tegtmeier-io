afe6ccdb4793d48709f40dfc361f99222ca9917d
jtegtmeier <jcbtegtmeier@gmail.com>
2017-04-05 15:21:12 -0700

    added contactform class and new project/about

--- README.md
+++ README.md
@@ -1,7 +1,15 @@
-#jake-tegtmeier-io
-###My personal portfolio project using express, webpack and react
+# jake-tegtmeier-io
+### My personal portfolio project using express, webpack and react
 
 * Based off [webpack-express-boilerplate](https://github.com/christianalfoni/webpack-express-boilerplate) by Christian Alfoni
-* Has a development and production environment
+* Responsive design, showcasing my projects and about page
+* Uses a development and production environment
 * Deployable to Heroku
 * Built a custom emailing component using react and express
+
+### Run
+
+* npm install
+* npm start (for development build)
+* npm run build (for production build)
+* npm run prod (for production build)
--- app/app.js
+++ app/app.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import 'whatwg-fetch';
-import validator from 'validator';
-import Recaptcha from 'react-gcaptcha';
 
 import styles from './style.css';
 import socstyles from './social-icons.css';
 import Scroll from 'react-scroll';
+import ContactForm from './contactForm.js';
 import FontAwesome from 'react-fontawesome';
 
 const Navlink    = Scroll.Link;
@@ -49,7 +48,7 @@ class App extends React.Component {
           <div className={styles.banner}>
             <h1 className={styles.bannertitle}>Jake.Tegtmeier.io</h1>
             <h2 className={styles.bannerdescription}>
-              (<span style={{color: '#63b9ff'}}>'Jake'</span>, <span style={{color: '#63b9ff'}}>'Tegtmeier'</span>)
+              <span style={{color: '#63b9ff'}}>=</span> ( )
               <span style={{color: '#deb284'}}> => </span>
               {'{'}<span style={{color: '#b878cb'}}>return</span> <span style={{color: '#91b65e'}}>  'Full-Stack Developer'</span>{'}'}
             </h2>
@@ -90,12 +89,16 @@ class App extends React.Component {
               <img src={require('./img/JSCalc9000.png')}/>
               <h3>JavaScript Calculator</h3>
             </a>
+            <a className={styles.imgtile} href="http://comment.tegtmeier.io">
+              <img src={require('./img/commentHub.png')}/>
+              <h3>React/Firebase Chat Board</h3>
+            </a>
           </div>
         </Element>
         <Element name="about" className={styles.about}>
           <h1>About</h1>
           <p>
-            I am a computer scientist deeply interested in the fields of web development, networking, and security. Designing web based software solutions with the user in mind is my main focus. There is nothing better then being able to holistically look at a problem, find an elegant model to represent it, and help your team work towards its explanation.
+            <b>Software Engineer</b> and <b>IT professional</b> interested in web development, computer networks, and information security. Designing scalable, responsive and secure software, with the user in mind, is my main focus. My work history is rich with web software development, routing protocols, network monitoring, and collaborative deployment of new computer networks. Currently my personal web projects revolve around responsive UI design, full-stack application development, and envisioning new patterns to make our apps more extendable, testable and secure. Languages, frameworks, and tools are not usually my main concern. Rather, I try to move forward with what works best for the situation based on the collective wisdom and iterative results of the team. I am always looking to connect with driven developers wanting to expand their understanding of CS fundamentals, web technologies, user experience, and information technology by building relationships, workflows, engaging software and ultimately new spaces for innovation.
           </p>
         </Element>
         <Element name="contact" className={styles.contact}>
@@ -119,175 +122,5 @@ class App extends React.Component {
     );
   }
 }
-
-class ContactForm extends React.Component{
-  constructor(props) {
-    super(props);
-    this.state = {
-      screen: 1,
-      formError: '',
-      formErrorStyle: {maxHeight: 0},
-      validEmail: false,
-      validSubject: false,
-      validMessage: false,
-      validCaptcha: false,
-      captchaLoaded: false,
-      email: '',
-      subject: '',
-      message: '',
-    };
-  }
-
-  emailEntered(event) {
-    if (!validator.isEmail(event.target.value)) {
-      this.setState({validEmail: false});
-    } else {
-      this.setState({validEmail: true});
-    }
-    this.setState({email: validator.escape(event.target.value)});
-  }
-
-  subjectEntered(event) {
-    const len = event.target.value.length;
-    if (len > 30 || len < 1) {
-      this.setState({validSubject: false});
-    } else {
-      this.setState({validSubject: true});
-      this.setState({subject: validator.escape(event.target.value)});
-    }
-  }
-
-  messageEntered(event) {
-    const len = event.target.value.length;
-    if (len > 300 || len < 1) {
-      this.setState({validMessage: false});
-    } else {
-      this.setState({validMessage: true});
-      this.setState({message: validator.escape(event.target.value)});
-    }
-  }
-
-  captchaSolved() {
-    this.setState({validCaptcha: true});
-  }
-
-  recaptchaDidLoad() {
-    this.setState({recaptchaLoaded: true});
-    console.log('Captcha Loaded!!!!');
-  }
-
-  allInputValid() {
-    if (!this.state.validEmail) {
-      this.setState({formError: 'Invalid Email.'});
-    } else if (!this.state.validSubject) {
-      this.setState({formError: 'Invalid Subject Text.'});
-    } else if (!this.state.validMessage) {
-      this.setState({formError: 'Invalid Message Text.'});
-    } else if (!this.state.validCaptcha) {
-      this.setState({formError: 'Captcha Not Solved. Try Refresh.'});
-    } else {
-      return true;
-    }
-    this.setState({formErrorStyle: {maxHeight: 60}});
-    window.setTimeout(function() { this.setState({formErrorStyle: {maxHeight: 0}});}.bind(this), 2000);
-    return false;
-  }
-
-  submitForm(event) {
-    if (this.allInputValid()) {
-      const data = {
-        email: this.state.email,
-        subject: this.state.subject,
-        message: this.state.message,
-      };
-
-      this.setState({screen: this.state.screen + 1});
-
-      console.log(data);
-      console.log(this.state);
-
-      fetch('/sendMail', {
-        method: 'POST',
-        headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-          email: this.state.email,
-          subject: this.state.subject,
-          message: this.state.message,
-        })
-      });
-    }
-  }
-
-  render() {
-    if (this.state.screen === 1) {
-      return (
-        <div className={styles.formContainer}>
-          <div className={styles.contactForm}>
-            <h4 className={styles.formLabel}>Send me a message.</h4>
-            <label className={styles.formError} style={this.state.formErrorStyle}>{this.state.formError}</label>
-            <div className={styles.formFields}>
-              <div className={styles.formField}>
-              <div>
-                <label for="emailField">Your Email:</label>
-                <input type="email" id="emailField" ref="email" className="form-control" defaultValue={this.props.email}
-                  onChange={this.emailEntered.bind(this)}
-                  value={this.state.email}/>
-              </div>
-                <div>
-                  <label for="subjectField">Subject:</label>
-                  <input type="text" id="subjectField" ref="subject" className="form-control" defaultValue={this.props.subject}
-                    onChange={this.subjectEntered.bind(this)}
-                    value={this.state.subject}/>
-                </div>
-              </div>
-              <div className={styles.formField}>
-                <div>
-                  <label for="messageField">Message:</label>
-                  <textarea type="text" id="messageField" ref="message" className="form-control" rows="3" cols="50" defaultValue={this.props.message}
-                    onChange={this.messageEntered.bind(this)}
-                    value={this.state.message}/>
-                </div>
-              </div>
-              <div className={styles.contactSubmit}>
-                <Recaptcha
-                  sitekey="6LfMpSITAAAAALAyAWseS__-4lm_DBlfJCj61_Ux"
-                  render="explicit"
-                  verifyCallback={this.captchaSolved.bind(this)}
-                  onloadCallback={this.recaptchaDidLoad.bind(this)}/>
-                <button className="btn" onClick={this.submitForm.bind(this)}>Submit</button>
-              </div>
-            </div>
-          </div>
-        </div>
-      );
-    } else if (this.state.screen === 2) {
-      return (
-        <div className={styles.contactSent}>
-          <h2>Message sent!</h2>
-          <h4>I'll get back to you as soon as I can.</h4>
-        </div>
-      );
-    } else {
-      return (
-        <div className={styles.contactError}>
-          <h2>Contact Form error.</h2>
-        </div>
-      );
-    }
-  }
-}
-ContactForm.propTypes = {
-  emailfield: React.PropTypes.string,
-  subjectfield: React.PropTypes.string,
-  messagefield: React.PropTypes.string,
-};
-ContactForm.defaultProps = {
-  emailfield: 'Email',
-  subjectfield: 'Subject',
-  messagefield: 'Message',
-};
 
 export default App;
